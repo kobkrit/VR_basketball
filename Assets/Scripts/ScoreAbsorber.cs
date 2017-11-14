97c2a00ea9ba8350b895299e7bb7d351762ab0ed
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2017-11-14 14:23:45 +0700

    Add files

---
+++ Assets/Scripts/ScoreAbsorber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreAbsorber : MonoBehaviour {
+
+	public int score = 0;
+
+	// Use this for initialization
+	void Start () {
+		ScoreCount oldCount = FindObjectOfType<ScoreCount> ();
+		score = 0;
+		if (oldCount) {
+			score = oldCount.score;
+			Destroy (oldCount.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/ScoreTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTrigger : MonoBehaviour {
+
+	public int scoreToIncrease = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider c){
+		ScoreCount scoreCount = FindObjectOfType<ScoreCount> ();
+		scoreCount.IncreaseScore (scoreToIncrease);
+	}
+}
